eeaac3a436ecb58241447787a859e7dcca670cc0
stmacdonell <7674911+stmacdonell@users.noreply.github.com>
2024-10-04 23:50:35 -0400

    Added attention selection, compilation, and fixed text/prompt mismatch

--- parler_tts_server/main.py
+++ parler_tts_server/main.py
@@ -22,7 +22,8 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
     logger.info("CPU will be used for inference")
-torch_dtype = torch.float16 if device != "cpu" else torch.float32
+#torch_dtype = torch.float16 if device != "cpu" else torch.float32
+torch_dtype = getattr(torch, config.torch_dtype.split('.')[-1])
 
 
 class ModelManager:
@@ -36,7 +37,10 @@ class ModelManager:
     ) -> tuple[ParlerTTSForConditionalGeneration, AutoTokenizer]:
         logger.debug(f"Loading {model_name}...")
         start = time.perf_counter()
-        model = ParlerTTSForConditionalGeneration.from_pretrained(model_name).to(  # type: ignore
+        model = ParlerTTSForConditionalGeneration.from_pretrained(
+            model_name,
+            attn_implementation=config.attn_implementation
+            ).to(  # type: ignore
             device,  # type: ignore
             dtype=torch_dtype,
         )
@@ -44,6 +48,34 @@ class ModelManager:
         logger.info(
             f"Loaded {model_name} and tokenizer in {time.perf_counter() - start:.2f} seconds"
         )
+
+        if config.compile_mode != "none":
+            # compile the forward pass
+            model.generation_config.cache_implementation = "static"
+            model.forward = torch.compile(model.forward, mode=compile_mode)
+
+            # warmup
+            inputs = tokenizer("This is for compilation", return_tensors="pt", padding="max_length", max_length=max_length).to(torch_device)
+
+            model_kwargs = {**inputs, "prompt_input_ids": inputs.input_ids, "prompt_attention_mask": inputs.attention_mask, }
+
+            # warmup
+            if compile_mode != "default":
+                    # generating more tokens than previously will trigger CUDA graphs capture
+                    # one should warmup with a number of generated tokens above max tokens targeted for subsequent generation
+                    model_kwargs = {
+                        "min_new_tokens": config.max_stream_sec*86,
+                        "max_new_tokens": config.max_stream_sec*86,
+                        **model_kwargs
+                    } 
+
+            n_steps = 1 if compile_mode == "default" else 2
+            for _ in range(n_steps):
+                start_time = time.time()
+                _ = model.generate(**model_kwargs)
+                elapsed_time = time.time()-start_time
+                print(f"Warm-up step took {elapsed_time:.2f} seconds")
+
         return model, tokenizer
 
     def get_or_load_model(
@@ -131,8 +163,8 @@ async def generate_audio(
             "Specifying speed isn't supported by this model. Audio will be generated with the default speed"
         )
     start = time.perf_counter()
-    input_ids = tokenizer(voice, return_tensors="pt").input_ids.to(device)
-    prompt_input_ids = tokenizer(input, return_tensors="pt").input_ids.to(device)
+    input_ids = tokenizer(input, return_tensors="pt").input_ids.to(device)
+    prompt_input_ids = tokenizer(voice, return_tensors="pt").input_ids.to(device)
     generation = tts.generate(
         input_ids=input_ids, prompt_input_ids=prompt_input_ids
     ).to(  # type: ignore
